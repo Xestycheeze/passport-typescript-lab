0c4b0ee15cf5413310d2bfd0299ad9c45d0be9e0
CBET <henryhung942000@gmail.com>
2025-10-24 03:28:02 -0700

    for accounts with admin role: - add admin page - lists all ongoing sessions - add ability to revoke any ongoing session

--- routes/indexRoute.ts
+++ routes/indexRoute.ts
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-import { ensureAuthenticated } from "../middleware/checkAuth";
+import { ensureAuthenticated, checkAdmin, getAllSessions } from "../middleware/checkAuth";
 
 router.get("/", (req, res) => {
   res.send("welcome");
@@ -12,4 +12,26 @@ router.get("/dashboard", ensureAuthenticated, (req, res) => {
   });
 });
 
+// router.get("/admin", checkAdmin, (req, res) => {
+//     const sessions = getAllSessions(req);
+//     res.render("admin", { sessions });
+// })
+
+// for some reasons this has to be async to have a pull payload.
+// code above should be a synchronous version of getAllSessions, but it doesn't work...
+// following code in collaboration with Hitoki
+router.get("/admin", checkAdmin, async (req, res) => {
+    const sessions = await getAllSessions(req);
+    res.render("admin", { sessions });
+})
+
+router.post("/admin/revoke/:sessionId", checkAdmin, (req, res) => {
+    const sessionId = req.params.sessionId;
+    req.sessionStore.destroy(sessionId, (err) => {
+        if(err){
+            throw new Error(err)
+        }
+        res.redirect("/admin")
+    })
+})
 export default router;
